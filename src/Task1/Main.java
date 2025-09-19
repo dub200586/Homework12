926ef3a6b572ab7ef155609e67cf88e049e51c01
konstantin.dubanevich <k.dubanevich@belitsoft.com>
2025-09-19 23:26:56 +0300

    Add Task1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,7 +1,24 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String string = sc.nextLine();
+        String[] words = string.replaceAll("[^A-zА-я ]", "")
+                .replaceAll(" +", " ").split(" ");
+        int firstWord = 0;
 
+        for (String word : words) {
+            if (word.matches("[A-ZА-Я]+") && word.length() >= 2 && word.length() <= 6) {
+                firstWord++;
+                if (firstWord == 1) {
+                    System.out.println("Аббревиатуры:");
+                }
+                System.out.println(word);
+            }
+        }
     }
 }
