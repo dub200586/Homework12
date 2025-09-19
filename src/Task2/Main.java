dc0f89b7d0b0544c37c25b99aae3fc58795a7923
konstantin.dubanevich <k.dubanevich@belitsoft.com>
2025-09-20 00:08:24 +0300

    Add Task2

---
+++ src/Task2/Main.java
@@ -0,0 +1,31 @@
+package Task2;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String string = sc.nextLine();
+
+        Pattern docNumberPattern = Pattern.compile("[\\d]{4}-[\\d]{4}-[\\d]{2}");
+        Pattern phoneNumberPattern = Pattern.compile("\\([\\d]{2}\\)[\\d]{7}");
+        Pattern emailPattern = Pattern.compile("[A-z\\d+_.-]+@[A-z\\d.-]+\\.[A-z]{2,}");
+
+        Matcher docNumber = docNumberPattern.matcher(string);
+        Matcher phoneNumber = phoneNumberPattern.matcher(string);
+        Matcher email = emailPattern.matcher(string);
+
+        if (docNumber.find()) {
+            System.out.println("Document number: " + docNumber.group());
+        }
+        if (phoneNumber.find()) {
+            System.out.println("Phone number: " + phoneNumber.group());
+        }
+        if (email.find()) {
+            System.out.println("Email: " + email.group());
+        }
+    }
+}
